19fb958d58f574d82e2cc62aa4d7be9291220034
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-08-20 22:20:42 +0700

    source code start admin 2 template

--- app/Enums/InvoiceStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum InvoiceStatus: string
-{
-    case PENDING = 'pending';
-    case SETTLEMENT = 'settlement';
-}
--- resources/js/Components/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Container({ children }) {
-    return (
-        <div className='max-w-screen-lg mx-auto px-4'>
-            {children}
-        </div>
-    )
-}
